2f070983f0947033215de98f9c54354b5289f239
Thomas Deconinck <137479@supinfo.com>
2014-06-23 22:03:24 +0200

    Update README + remove useless instanciation

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 var http = require('http'),
 Smart    = require('./smart');
 
-var App = new Smart();
-
-App.Router.map("/hello");
+Smart.Router.map("/hello");
 
 http.createServer(function(request, response) {
-	App.Router.manage(request, response);
+	Smart.Router.manage(request, response);
 }).listen(3000);
 
 console.log("Server launched on localhost:3000 ...");
--- smart.js
+++ smart.js
@@ -4,4 +4,5 @@ var Smart = function() {
 	this.Router = new Router();
 };
 
-module.exports = Smart;
+
+module.exports = new Smart();
