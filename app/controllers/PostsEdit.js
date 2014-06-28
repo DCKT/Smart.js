81099c2bc2a347db29ebc3faea6ac26bac98c3ac
Thomas Deconinck <137479@supinfo.com>
2014-06-28 23:57:40 +0200

    Add optional Controller

--- app/controllers/PostsEdit.js
+++
@@ -1,5 +0,0 @@
-var controller = require('../../lib/smart').Controller;
-
-module.exports = controller.extend(function(req, res) {
-	console.log(req.params);
-});
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http'),
 Smart    = require('./lib/smart');
 
-Smart.Router.map("/hello", "/posts/new", "/posts/:id", "/posts/:id/edit");
+// Smart.Router.map("/hello", "/posts/new", "/posts/:id", "/posts/:id/edit");
 
 http.createServer(function(request, response) {
 	Smart.Router.manage(request, response);
